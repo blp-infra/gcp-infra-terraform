0b36b14c6fa104d37dd6336b3c283ca6969512dd
manupanand <manupanandindianinvestor@gmail.com>
2025-08-26 10:19:48 +0530

    new code update

--- terraform_code/main.tf
+++ terraform_code/main.tf
@@ -1,10 +1,11 @@
 module "project" {
-  source          = "./project"
-  for_each        = var.projects
-  project_id      = each.value["project_id"]
-  project_name    = each.value["name"]
-  organization_id = each.value["organization_id"]
-  billing_account = each.value["billing_account"]
+  source              = "./project"
+  for_each            = var.projects
+  project_id          = each.value["project_id"]
+  project_name        = each.value["name"]
+  organization_id     = each.value["organization_id"]
+  billing_account     = each.value["billing_account"]
+  
 }
 # module "vpc" {
 #   source = "./vpc"
--- terraform_code/project/main.tf
+++ terraform_code/project/main.tf
@@ -1,6 +1,7 @@
 resource "google_project" "infra_project" {
-  name            = var.project_name
-  project_id      = var.project_id
+  name                = var.project_name
+  project_id          = var.project_id
   #org_id     = var.organization_id // under which organization
-  billing_account = var.billing_account
+  billing_account     = var.billing_account
+  auto_create_network = false
 }
